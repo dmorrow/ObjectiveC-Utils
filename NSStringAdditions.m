d891461891d04aad9c956712ff1823624da709ee
Danny Morrow <danny@unitytheory.com>
2015-05-07 16:31:48 -0400

    silence xcode warnings

--- NSStringAdditions.m
+++ NSStringAdditions.m
@@ -43,7 +43,7 @@
 {
     const char* str = [self UTF8String];
     unsigned char result[CC_MD5_DIGEST_LENGTH];
-    CC_MD5(str, strlen(str), result);
+    CC_MD5(str, (CC_LONG)strlen(str), result);
     
     NSMutableString *ret = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH*2];
     for(int i = 0; i<CC_MD5_DIGEST_LENGTH; i++) {
